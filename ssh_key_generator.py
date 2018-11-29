90f3898704551b269dd47f937f9035bd1c282d89
YaroslavHimko <yaroslav.himko@gmail.com>
2018-11-29 23:21:03 +0300

    Refactored code to use OOP principles

--- ssh_key_generator.py
+++ ssh_key_generator.py
@@ -4,51 +4,73 @@ import json
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-with open("config.json") as f:
-    data = json.load(f)
-    ips = data["ips"]
-    port = data["port"]
-    username = data["username"]
-    password = data["password"]
+
+def file_opener(config):
+    with open(config) as f:
+        data = json.load(f)
+        return data
 
 
-    def key_present():
-        stdin, stdout, stderr = ssh.exec_command("ls /home/{}/.ssh".format(username))
+class Host(object):
+    def __init__(self, host_machine):
+        self.ip = host_machine["ip"]
+        self.port = host_machine["port"]
+        self.username = host_machine["username"]
+        self.password = host_machine["password"]
+
+    def key_present(self):
+        """
+        Returns True if ssh key already exists, False if ssh key was not found.
+        """
+        stdin, stdout, stderr = ssh.exec_command("ls ~/.ssh")
         for line in stdout.readlines():
             line.strip
 
             if "id_rsa" in line:
-                print("Key exists")
+                print("Key exists for {}".format(self.username))
                 return True
             else:
                 return False
 
+    def generate_key(self):
+        """
+        Generates ssh key for a host machine.
+        """
+        print("Generating key for {}".format(self.username))
+        ssh.exec_command('ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""')
+        ssh.exec_command("chmod 700 ~/.ssh | chmod 600 ~/.ssh/id_rsa ")
 
-def generate_key(user):
-    print("Generating key")
-    ssh.exec_command('ssh-keygen -t rsa -f /home/{}/.ssh/id_rsa -q -P ""'.format(user))
-    ssh.exec_command("chmod 700 ~/.ssh | chmod 600 ~/.ssh/id_rsa ")
+    def remove_key(self):
+        """
+        Removes ssh key for a host machine.
+        """
+        print("Removing key for {}".format(self.username))
+        ssh.exec_command("rm -f ~/.ssh/id_rsa")
+        ssh.exec_command("rm -f ~/.ssh/id_rsa.pub")
 
+    def connect_to_host(self):
+        """
+        Establishes connection with a host machine.
+        """
+        ssh.connect(hostname=self.ip, port=self.port, username=self.username, password=self.password)
 
-def remove_key(user):
-    print("Removing key")
-    ssh.exec_command("rm -f /home/{}/.ssh/id_rsa".format(user))
-    ssh.exec_command("rm -f /home/{}/.ssh/id_rsa.pub".format(user))
-
-
-def connect_to_linux(ip):
-    ssh.connect(hostname=ip, port=port, username=username, password=password)
-
-    if not key_present():
-        generate_key(username)
-    else:
-        remove_key(username)
+    def reboot(self):
+        """
+        Reboots host machine
+        """
+        print("Rebooting host {}".format(self.ip))
+        ssh.exec_command("reboot")
 
 
 def main():
     try:
-        for ip in ips:
-            connect_to_linux(ip)
+        config = file_opener("config.json")
+        for host_machine in config["Host"]:
+            host = Host(host_machine)
+            host.connect_to_host()
+            if not host.key_present():
+                host.generate_key()
+            host.reboot()
 
     except paramiko.SSHException:
         print("Connection Failed")
